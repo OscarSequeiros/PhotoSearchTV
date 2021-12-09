4f50e782114fa486b28b8ded8a51549366fa797d
Oscar Sequeiros <oscar.sequeiros.serrano@gmail.com>
2021-12-09 04:35:46 -0500

    security: Extracting api key to build.gradle file

--- app/src/main/java/com/osequeiros/photosearchtv/data/remote/RemotePhotosApi.kt
+++ app/src/main/java/com/osequeiros/photosearchtv/data/remote/RemotePhotosApi.kt
@@ -1,14 +1,20 @@
 package com.osequeiros.photosearchtv.data.remote
 
+import com.osequeiros.photosearchtv.BuildConfig
 import com.osequeiros.photosearchtv.data.remote.model.RemotePhotosResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RemotePhotosApi {
 
-    @GET("services/rest/?method=flickr.photos.getRecent&api_key=b6cb1608a7baa2e74fc6787d410bc0de&extras=date_taken,url_n,url_o,owner_name&&format=json&nojsoncallback=1")
-    suspend fun recent(): RemotePhotosResponse
+    @GET("services/rest/?method=flickr.photos.getRecent&extras=date_taken,url_n,url_o,owner_name&&format=json&nojsoncallback=1")
+    suspend fun recent(
+        @Query("api_key") apiKey: String = BuildConfig.FLICKER_API_KEY
+    ): RemotePhotosResponse
 
-    @GET("services/rest/?method=flickr.photos.search&api_key=b6cb1608a7baa2e74fc6787d410bc0de&extras=date_taken,url_n,url_o,owner_name&&format=json&nojsoncallback=1")
-    suspend fun search(@Query("text") value: String): RemotePhotosResponse
+    @GET("services/rest/?method=flickr.photos.search&extras=date_taken,url_n,url_o,owner_name&&format=json&nojsoncallback=1")
+    suspend fun search(
+        @Query("api_key") apiKey: String = BuildConfig.FLICKER_API_KEY,
+        @Query("text") value: String
+    ): RemotePhotosResponse
 }
